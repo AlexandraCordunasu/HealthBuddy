0319ef3664242b4d115c0e3d067b4b7e519f7289
fugulinmaria <mariafugulin@gmail.com>
2022-12-27 13:35:52 +0200

    form

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
 import kivy
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
 
-class RandomNumber(App):
-  def build(self):
-    return Label(text="Random Number Generator")
+class MyGrid(GridLayout):
+    def __init__ (self, **kwargs):
+        super(MyGrid, self).__init__(**kwargs)
+        self.cols = 2
+        self.add_widget(Label(text="First Name: "))
+        self.name = TextInput(multiline=False)
+        self.add_widget(self.name)
 
-randomApp = RandomNumber()
-randomApp.run()
+        self.add_widget(Label(text="Last Name: "))
+        self.lastName = TextInput(multiline=False)
+        self.add_widget(self.lastName)
+
+        self.add_widget(Label(text="Email: "))
+        self.email = TextInput(multiline=False)
+        self.add_widget(self.email)
+
+class MyApp(App):
+    def build(self):
+        return MyGrid()
+
+if __name__ == "__main__":
+    MyApp().run()
